6c703737a8dd636da236a5466dd67e1387748ec5
Klodian Kambo <klodian.kambo@molo17.com>
2021-03-14 13:44:09 +0100

    Implement error parsing, conversion and mapping. Show errors to user

--- app/src/main/java/klodian/kambo/weather/MainViewModel.kt
+++ app/src/main/java/klodian/kambo/weather/MainViewModel.kt
@@ -1,9 +1,12 @@
 package klodian.kambo.weather
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import arrow.core.Either
+import klodian.kambo.domain.GetValidSearchPatternUseCase
 import klodian.kambo.domain.Weather
 import klodian.kambo.domain.WeatherRepo
 import kotlinx.coroutines.Dispatchers
@@ -12,20 +15,38 @@ import java.util.*
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    private val weatherRepo: WeatherRepo
+    private val weatherRepo: WeatherRepo,
+    private val getValidSearchPatternUseCase: GetValidSearchPatternUseCase
 ) : ViewModel() {
 
-    private val weatherLiveData: MutableLiveData<List<UiWeather>> = MutableLiveData()
-
-    fun getWeather(pattern: String, locale: Locale): LiveData<List<UiWeather>> {
-        viewModelScope.launch(Dispatchers.IO) {
-            weatherLiveData.postValue(
-                weatherRepo.getWeather(pattern.trim(), locale)
-                    .map { it.toUiWeather() })
-        }
-        return weatherLiveData
+    sealed class SearchInputError(@StringRes val errorMessageResId: Int) {
+        object FieldCannotBeNull : SearchInputError(R.string.search_input_error_empty)
+        object Only3ParamsAreAllowed : SearchInputError(R.string.search_input_error_too_many_params)
+        object PleaseInsertTheCity : SearchInputError(R.string.search_input_error_no_param_found)
     }
 
+    private val weatherLiveData: MutableLiveData<Either<SearchInputError, List<UiWeather>>> =
+        MutableLiveData()
+
+    fun getWeatherResult(): LiveData<Either<SearchInputError, List<UiWeather>>> = weatherLiveData
+
+    fun getWeather(pattern: String, locale: Locale) {
+        getValidSearchPatternUseCase(pattern).fold(
+            ifRight = {
+                viewModelScope.launch(Dispatchers.IO) {
+                    weatherLiveData.postValue(
+                        Either.right(weatherRepo.getWeather(pattern.trim(), locale)
+                            .map { it.toUiWeather() })
+                    )
+                }
+            },
+            ifLeft = { patternValidationError ->
+                weatherLiveData.postValue(Either.left(patternValidationError.toSearchInputError()))
+            })
+    }
+
+
+    // Private fun
     private fun Weather.toUiWeather(): UiWeather {
         return UiWeather(
             id = id,
@@ -34,4 +55,11 @@ class MainViewModel @Inject constructor(
             iconPath = iconName
         )
     }
+
+    private fun GetValidSearchPatternUseCase.PatternValidationError.toSearchInputError(): SearchInputError =
+        when (this) {
+            GetValidSearchPatternUseCase.PatternValidationError.NullOrEmptyPattern -> SearchInputError.FieldCannotBeNull
+            GetValidSearchPatternUseCase.PatternValidationError.TooManyCommaParams -> SearchInputError.Only3ParamsAreAllowed
+            GetValidSearchPatternUseCase.PatternValidationError.NoParamsFound -> SearchInputError.PleaseInsertTheCity
+        }
 }
